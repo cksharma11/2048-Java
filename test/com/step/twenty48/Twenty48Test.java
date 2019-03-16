70214617d692ee8eebee41b6177bffc200330749
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-03-16 22:49:20 +0530

    Added package structure for tests

---
+++ test/com/step/twenty48/Twenty48Test.java
@@ -0,0 +1,65 @@
+package com.step.twenty48;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+class Twenty48Test {
+
+    Twenty48 twenty48;
+    @BeforeEach
+    void setUp() {
+        twenty48 = new Twenty48(3);
+    }
+
+    @Test
+    void initBoard() {
+        List<List<Integer>> initialBoard = new ArrayList<>();
+
+        initialBoard.add(Arrays.asList(0,0,0));
+        initialBoard.add(Arrays.asList(0,0,0));
+        initialBoard.add(Arrays.asList(0,0,0));
+
+    }
+
+    @Test
+    void printBoard() {
+    }
+
+    @Test
+    void getNewCellNumbers() {
+    }
+
+    @Test
+    void getRandom() {
+    }
+
+    @Test
+    void getNewCellPositions() {
+    }
+
+    @Test
+    void updateBoard() {
+    }
+
+    @Test
+    void moveLeft() {
+    }
+
+    @Test
+    void moveRight() {
+    }
+
+    @Test
+    void moveUp() {
+    }
+
+    @Test
+    void moveDown() {
+    }
+}
